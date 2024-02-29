30a513f57dbf70832d454eda2984ed5c66fd21c7
JiamingValeo <jiaming1.zhang@valeo.com>
2024-02-29 11:17:59 +0100

    ocpp type update

--- afb-types/src/ocpp-types.rs
+++ afb-types/src/ocpp-types.rs
@@ -10,94 +10,95 @@
  *
  */
 
-use afbv4::prelude::*;
-use crate::prelude::*;
-use serde::{Deserialize, Serialize};
-
-AfbDataConverter!(ocpp_msg, OcppMsg);
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "lowercase")]
-pub enum OcppMsg {
-    Initialized,
-    Reset,
-    Authorized(bool),
-    Transaction(bool,u32),
-    Reservation(ReservationSession),
-    PowerLimit(PowerLimit),
-    RemoteStopTransaction(bool),
-    Unknown,
-}
-
-AfbDataConverter!(ocpp_transaction, OcppTransaction);
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "lowercase")]
-pub enum OcppTransaction {
-    Start(String),
-    Stop(i32),
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "lowercase")]
-pub enum OcppErrorCode {
-    ConnectorLockFailure,
-    GroundFailure,
-    HighTemperature,
-    InternalError,
-    NoError,
-    OtherError,
-    OverCurrentFailure,
-    OverVoltage,
-    PowerMeterFailure,
-    PowerSwitchFailure,
-    ReaderFailure,
-    UnderVoltage,
-    WeakSignal,
-}
-
-AfbDataConverter!(ocpp_charger_status, OcppChargerStatus);
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "lowercase")]
-pub enum OcppChargerStatus {
-    Preparing,
-    Charging,
-    Reserved,
-    Unavailable,
-    Available,
-    Finishing,
-    Error(OcppErrorCode),
-}
-
-
-AfbDataConverter!(ocpp_state, OcppState);
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "lowercase")]
-pub struct OcppState {
-    pub connector_id: u32,
-    pub tid: i32,
-    pub reservation: Option<ReservationSession>,
-    pub authorized: bool,
-    pub status: OcppChargerStatus,
-    pub remote_stop_transaction: bool,
-}
-
-impl OcppState {
-    pub fn default() -> Self {
-        OcppState {
-            connector_id: 0,
-            tid: 0,
-            reservation: None,
-            authorized: false,
-            status: OcppChargerStatus::Preparing,
-            remote_stop_transaction: true, //true because chmgr api is opposite - remotepower
-        }
-    }
-}
-
-pub fn ocpp_registers() -> Result<(), AfbError> {
-    ocpp_msg::register()?;
-    ocpp_state::register()?;
-    ocpp_transaction::register()?;
-    ocpp_charger_status::register()?;
-
-    Ok(())
-}
+ use afbv4::prelude::*;
+ use crate::prelude::*;
+ use serde::{Deserialize, Serialize};
+ 
+ AfbDataConverter!(ocpp_msg, OcppMsg);
+ #[derive(Serialize, Deserialize, Debug, Clone)]
+ #[serde(rename_all = "lowercase")]
+ pub enum OcppMsg {
+     Initialized,
+     Reset,
+     Authorized(bool),
+     Transaction(bool,u32),
+     Reservation(ReservationSession),
+     PowerLimit(PowerLimit),
+     remote_stop_transaction(bool),
+     Unknown,
+ }
+ 
+ AfbDataConverter!(ocpp_transaction, OcppTransaction);
+ #[derive(Serialize, Deserialize, Debug, Clone)]
+ #[serde(rename_all = "lowercase")]
+ pub enum OcppTransaction {
+     Start(String),
+     Stop(i32),
+ }
+ 
+ #[derive(Serialize, Deserialize, Debug, Clone)]
+ #[serde(rename_all = "lowercase")]
+ pub enum OcppErrorCode {
+     ConnectorLockFailure,
+     GroundFailure,
+     HighTemperature,
+     InternalError,
+     NoError,
+     OtherError,
+     OverCurrentFailure,
+     OverVoltage,
+     PowerMeterFailure,
+     PowerSwitchFailure,
+     ReaderFailure,
+     UnderVoltage,
+     WeakSignal,
+ }
+ 
+ AfbDataConverter!(ocpp_charger_status, OcppChargerStatus);
+ #[derive(Serialize, Deserialize, Debug, Clone)]
+ #[serde(rename_all = "lowercase")]
+ pub enum OcppChargerStatus {
+     Preparing,
+     Charging,
+     Reserved,
+     Unavailable,
+     Available,
+     Finishing,
+     Error(OcppErrorCode),
+ }
+ 
+ 
+ AfbDataConverter!(ocpp_state, OcppState);
+ #[derive(Serialize, Deserialize, Debug, Clone)]
+ #[serde(rename_all = "lowercase")]
+ pub struct OcppState {
+     pub connector_id: u32,
+     pub tid: i32,
+     pub reservation: Option<ReservationSession>,
+     pub authorized: bool,
+     pub status: OcppChargerStatus,
+     pub remote_stop_transaction: bool,
+ }
+ 
+ impl OcppState {
+     pub fn default() -> Self {
+         OcppState {
+             connector_id: 0,
+             tid: 0,
+             reservation: None,
+             authorized: false,
+             status: OcppChargerStatus::Preparing,
+             remote_stop_transaction: true, //true because chmgr api is opposite - remotepower
+         }
+     }
+ }
+ 
+ pub fn ocpp_registers() -> Result<(), AfbError> {
+     ocpp_msg::register()?;
+     ocpp_state::register()?;
+     ocpp_transaction::register()?;
+     ocpp_charger_status::register()?;
+ 
+     Ok(())
+ }
+ 

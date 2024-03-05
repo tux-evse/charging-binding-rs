9a590d1d82f2bc0912ba244a490f89e4a659ad97
romain-valeo <romain.munier@valeo.com>
2024-03-05 11:13:58 +0100

    logout transaction

--- chmgr-lib/src/manager.rs
+++ chmgr-lib/src/manager.rs
@@ -203,7 +203,7 @@ impl ManagerHandle {
                 afb_log_msg!(
                     Warning,
                     evt,
-                    "ocpp reservation status:{:?}",
+                    "ocpp reservation staus:{:?}",
                     reservation.status
                 );
                 self.reserve(reservation)?;
@@ -218,6 +218,7 @@ impl ManagerHandle {
                 // new event for re mote stop
                 afb_log_msg!(Warning, evt, "ocpp transaction power:{} received tid:{}", status, tid);
                 AfbSubCall::call_sync(self.apiv4, self.iec_api, "power", *status)?;
+                AfbSubCall::call_sync(evt.get_api(), self.auth_api, "logout", data.total)?;
                 data_set.power = PowerRequest::Idle;
             }
 
